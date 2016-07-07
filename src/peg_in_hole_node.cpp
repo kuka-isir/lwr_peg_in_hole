a15f10896ce320fd720d6fe5e77ae63fe956a9e0
JimmyDaSilva <jimmy.dasilva@isir.upmc.fr>
2016-07-07 17:00:33 +0200

    New scenario

--- src/peg_in_hole_node.cpp
+++ src/peg_in_hole_node.cpp
@@ -5,13 +5,12 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "peg_in_hole_node");
 
   PegInHole peg_in_hole;
+  peg_in_hole.loadHolesLocation("plaque2");
+
   peg_in_hole.moveToStart();
-
-  peg_in_hole.loadHolesLocation("plaque1.stl");
-
-  peg_in_hole.moveAboveObjectHole("plaque1.stl", 0);
-  peg_in_hole.moveAboveObjectHole("plaque1.stl", 1);
-  peg_in_hole.moveAboveObjectHole("plaque1.stl", 2);
+  peg_in_hole.moveAboveObjectHole("plaque2", 0);
+  peg_in_hole.moveToStart();
+  peg_in_hole.moveAboveObjectHole("plaque2", 1);
 
   ros::shutdown();
   return 0;
