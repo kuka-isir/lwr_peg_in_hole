ebcb8beb4cc9a27bfb72a91f42adf9ded00eed38
JimmyDaSilva <jimmy.dasilva@isir.upmc.fr>
2016-09-05 11:39:28 +0200

    Improving over all demo script

--- src/real_demo_test0.cpp
+++ src/real_demo_test0.cpp
@@ -35,6 +35,9 @@ int main(int argc, char **argv)
   lwr_peg_in_hole::EstimateHolesServiceResponse resp_estimate;
   req_estimate.object_name = "plaque2";
   
+  lwr_peg_in_hole::FindHolePoseServiceRequest req_find;
+  lwr_peg_in_hole::FindHolePoseServiceResponse resp_find;
+  
   std::vector<double> look_up1, look_up2, look_up3;
   look_up1.push_back(1.64647737821113);
   look_up1.push_back(-0.06401549522493077);
@@ -60,55 +63,88 @@ int main(int argc, char **argv)
   look_up3.push_back(1.3746182539883787);
   look_up3.push_back(-0.910279200952389);
 
-
+  /*********** DEMO script *****************/
+  // Go to start position
   while(!robot_move.moveToStart() && ros::ok()){usleep(1E6);}
+  
+  // Go to lookup position
   while(!robot_move.moveToJointPosition(look_up1) && ros::ok()){usleep(1E6);}
 
+  // Wait 2s
   usleep(2E6);
   
+  // Update object position in moveit planning scene
   while(!up_client.call(req_update,resp_update) && ros::ok())
   {
       usleep(1E6);
-      ROS_INFO("Waiting for object");
+      ROS_INFO("Waiting for object...");
   }
-
+  
+  // Read holes poses
+  robot_move.loadHolesLocation("plaque2");
   while(!estimate_client.call(req_estimate,resp_estimate) && ros::ok())
   {
       usleep(1E6);
-      ROS_INFO("Getting the holes pose estimate");
+      ROS_INFO("Getting the holes pose estimate...");
   }
   
-  robot_move.loadHolesLocation("plaque2");
+  // Move to above first hole
   while(!robot_move.moveAboveObjectHole("plaque2", 0) && ros::ok()){usleep(1E6);}
   
-
-//   geometry_msgs::Pose above_hole = resp_estimate.holes_poses[0];
-//   above_hole.position.z += 0.45;
-//   above_hole.orientation.x = 1.0;
-//   above_hole.orientation.y = 0;
-//   above_hole.orientation.z = 0;
-//   above_hole.orientation.w = 0;
-//   
-//   sensor_msgs::JointState above_hole_jnts;
-//   robot_move.compute_ik(above_hole,above_hole_jnts);
-//   std::vector<double> above_hole_jnts_vector;
-//   
-//   for(int i=0; i<above_hole_jnts.position.size();i++){
-//     above_hole_jnts_vector.push_back(above_hole_jnts.position[i]);
-//   }
-//   std::cout<<"Pose \n" <<above_hole <<std::endl;
-//   
-//   std::cout<<"Joints \n" <<above_hole_jnts <<std::endl;
-//   while(!robot_move.moveToJointPosition(above_hole_jnts_vector) && ros::ok()){usleep(1E6);}
-
-
-
+  // Get closer to the hole
   geometry_msgs::Pose test_lin_rel;
   test_lin_rel.orientation.w = 1.0;
   test_lin_rel.position.z = -robot_move.dist_above_hole_/2.0;
   robot_move.moveLinRel(test_lin_rel);
+ 
+  // Wait 2s to stabilize camera image
+  usleep(2E6);
+  
+  // Update object position in moveit planning scene
+  req_find.hole_pose_estimate = resp_estimate.holes_poses[0];
+  while(!find_client.call(req_find,resp_find) && ros::ok())
+  {
+      usleep(1E6);
+      ROS_INFO("Looking for real hole pose...");
+  }
+  std::cout << "Estimate pose\n" << req_find.hole_pose_estimate << std::endl;
+  std::cout << "Correct pose\n" << resp_find.hole_pose << std::endl;
+  
+  
+  // Correct pose
+  resp_find.hole_pose.position.z += robot_move.dist_above_hole_/2.0;
+  
+  tf::Transform hole_transform;
+  hole_transform.setOrigin(tf::Vector3(resp_find.hole_pose.position.x, resp_find.hole_pose.position.y, resp_find.hole_pose.position.z));
+  hole_transform.setRotation(tf::Quaternion(resp_find.hole_pose.orientation.x, resp_find.hole_pose.orientation.y, resp_find.hole_pose.orientation.z, resp_find.hole_pose.orientation.w));
 
+  tf::Transform pi_rotation_transform;
+  pi_rotation_transform.setOrigin(tf::Vector3(0.0, 0.0, 0.0));
+  tf::Quaternion pi_rotation;
+  pi_rotation.setRPY(M_PI,0,0);
+  pi_rotation_transform.setRotation(pi_rotation);
 
+  hole_transform *= pi_rotation_transform;
+  
+  resp_find.hole_pose.position.x = hole_transform.getOrigin().getX();
+  resp_find.hole_pose.position.y = hole_transform.getOrigin().getY();
+  resp_find.hole_pose.position.z = hole_transform.getOrigin().getZ();
+//   resp_find.hole_pose.orientation.x = hole_transform.getRotation().getX();
+//   resp_find.hole_pose.orientation.y = hole_transform.getRotation().getY();
+//   resp_find.hole_pose.orientation.z = hole_transform.getRotation().getZ();
+//   resp_find.hole_pose.orientation.w = hole_transform.getRotation().getW();
+  
+    // HACK !!!!!!!!!!
+  tf::Quaternion perfect_orientation;
+  perfect_orientation.setRPY(-M_PI,0,-(M_PI/2.0 + 20*M_PI/180.0));
+  resp_find.hole_pose.orientation.x = perfect_orientation.x();
+  resp_find.hole_pose.orientation.y = perfect_orientation.y();
+  resp_find.hole_pose.orientation.z = perfect_orientation.z();
+  resp_find.hole_pose.orientation.w = perfect_orientation.w();
+  
+  
+  while(!robot_move.moveToCartesianPose(resp_find.hole_pose) && ros::ok()){usleep(1E6);}
+  
   ros::shutdown();
   return 0;
 }
